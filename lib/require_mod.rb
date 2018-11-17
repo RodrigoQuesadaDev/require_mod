9588401267a5b26c7395a4f77902a74acb52fddd
Rodrigo Quesada <rodrigoquesada.dev@gmail.com>
2018-11-16 23:41:17 -0600

    Improved code isolation.

--- lib/require_mod.rb
+++ lib/require_mod.rb
@@ -3,41 +3,48 @@ require 'pathname'
 
 module RequireMod
 
-  module Regex
-    RUBY_FILE = /\.rb$/
+  module Application
+
+    module Regex
+      RUBY_FILE = /\.rb$/
+    end
+    private_constant :Regex
+
+    class << self
+      def _require_single_mod(mod)
+        require_children Pathname.new('lib') / mod
+      end
+
+      private
+
+      def require_children dir
+        mod_file = dir / 'module.rb'
+        require mod_file.expand_path if mod_file.exist?
+
+        files, sub_dirs = dir.children.sort.partition(&:file?)
+
+        files.to_enum
+            .map { |it| it.expand_path.to_s }
+            .grep(Regex::RUBY_FILE)
+            .each { |it| require it }
+
+        sub_dirs.each { |it| require_children it }
+      end
+
+      def require(file)
+        Kernel.send(:require, file)
+      end
+    end
   end
-  private_constant :Regex
 
-  class << self
-    def _require_single_mod(mod)
-      require_children Pathname.new('lib') / mod
+  module Api
+    module_function
+
+    def require_mod(*mods)
+      mods.each { |it| RequireMod::Application._require_single_mod it }
+      nil
     end
-
-    private
-
-    def require_children dir
-      mod_file = dir / 'module.rb'
-      require mod_file.expand_path if mod_file.exist?
-
-      files, sub_dirs = dir.children.sort.partition(&:file?)
-
-      files.to_enum
-          .map { |it| it.expand_path.to_s }
-          .grep(Regex::RUBY_FILE)
-          .each { |it| require it }
-
-      sub_dirs.each { |it| require_children it }
-    end
-
-    def require(file)
-      Kernel.send(:require, file)
-    end
-  end
-
-  def require_mod(*mods)
-    mods.each { |it| RequireMod._require_single_mod it }
-    nil
   end
 end
 
-include RequireMod
+extend RequireMod::Api
